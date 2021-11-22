10d5412426bf3ba4d92fccbd235c9289e113982b
yuki fujisawa <fujisawa.yuki@outlook.com>
2021-11-22 11:36:58 +0900

    投稿画像にresize_to_limit設定

--- app/controllers/pictures_controller.rb
+++ app/controllers/pictures_controller.rb
@@ -1,35 +1,33 @@
 class PicturesController < ApplicationController
-  before_action :set_picture, only: %i[ show edit update destroy ]
+  before_action :set_picture, only: %i[show edit update destroy]
 
   def index
     @pictures = Picture.all
   end
 
-  def show
-  end
+  def show; end
 
   def new
-    if params[:back]
-    @picture = Picture.new(picture_params)
-    else
-    @picture = Picture.new
-    end
+    @picture = if params[:back]
+                 Picture.new(picture_params)
+               else
+                 Picture.new
+               end
   end
 
   def confirm
     @picture = current_user.pictures.build(picture_params)
     render :new if @picture.invalid?
   end
-  
-  def edit
-  end
+
+  def edit; end
 
   def create
     @picture = Picture.new(picture_params)
 
     respond_to do |format|
       if @picture.save
-        format.html { redirect_to @picture, notice: "Picture was successfully created." }
+        format.html { redirect_to @picture, notice: 'Picture was successfully created.' }
         format.json { render :show, status: :created, location: @picture }
       else
         format.html { render :new, status: :unprocessable_entity }
@@ -41,7 +39,7 @@ class PicturesController < ApplicationController
   def update
     respond_to do |format|
       if @picture.update(picture_params)
-        format.html { redirect_to @picture, notice: "Picture was successfully updated." }
+        format.html { redirect_to @picture, notice: 'Picture was successfully updated.' }
         format.json { render :show, status: :ok, location: @picture }
       else
         format.html { render :edit, status: :unprocessable_entity }
@@ -53,12 +51,13 @@ class PicturesController < ApplicationController
   def destroy
     @picture.destroy
     respond_to do |format|
-      format.html { redirect_to pictures_url, notice: "Picture was successfully destroyed." }
+      format.html { redirect_to pictures_url, notice: 'Picture was successfully destroyed.' }
       format.json { head :no_content }
     end
   end
 
   private
+
   def set_picture
     @picture = Picture.find(params[:id])
   end
