019a3e424b62ccf0d6cae798ca331648a12c9109
yuki fujisawa <fujisawa.yuki@outlook.com>
2021-11-22 14:47:33 +0900

    自動生成しない設定追加

--- config/application.rb
+++ config/application.rb
@@ -1,13 +1,15 @@
 require_relative 'boot'
-
 require 'rails/all'
-
 Bundler.require(*Rails.groups)
-
 module IgCloneApp
   class Application < Rails::Application
     config.load_defaults 6.0
     config.time_zone = 'Tokyo'
     config.active_record.default_timezone = :local
+    config.i18n.default_locale = :ja
+    config.generators do |g|
+      g.assets false
+      g.helper false
+    end
   end
 end
