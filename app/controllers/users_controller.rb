aaec02beb728f35fbb25a91f11b2fda1c1705b9c
yuki fujisawa <fujisawa.yuki@outlook.com>
2021-11-23 15:36:33 +0900

    userのプロフィール画像投稿機能追加

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,5 @@
 class UsersController < ApplicationController
-  skip_before_action :login_required, only: [:new, :create]
+  skip_before_action :login_required, only: [:new, :create, :edit]
 def new
   @user = User.new
 end
@@ -14,9 +14,24 @@ end
 def show
   @user = User.find(params[:id])
 end
+def edit
+  @user = User.find(params[:id])
+  if @user == current_user
+    render :edit
+  else
+    redirect_to users_path
+  end
+end
+def update
+  @user = User.find(params[:id])
+  if @user.update(user_params)
+    redirect_to user_path, notice: 'user was successfully updated.'
+    render :edit
+  end
+end
 
 private
 def user_params
-  params.require(:user).permit(:name, :email, :password, :password_confirmation)
+  params.require(:user).permit(:name, :email, :password, :password_confirmation, :image, :image_cache)
 end
 end
