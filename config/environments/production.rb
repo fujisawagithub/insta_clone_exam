3b14cb3ff23d68411240b65f45c937e443c1acb9
yuki fujisawa <fujisawa.yuki@outlook.com>
2021-11-25 11:35:32 +0900

    fix

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -12,12 +12,12 @@ Rails.application.configure do
   config.i18n.fallbacks = true
   config.active_support.deprecation = :notify
   config.log_formatter = ::Logger::Formatter.new
+  config.action_mailer.default_url_options = { host: 'https://morning-tundra-66078.herokuapp.com/'}
+  config.action_mailer.delivery_method = :letter_opener_web
   if ENV["RAILS_LOG_TO_STDOUT"].present?
     logger           = ActiveSupport::Logger.new(STDOUT)
     logger.formatter = config.log_formatter
     config.logger    = ActiveSupport::TaggedLogging.new(logger)
   end
   config.active_record.dump_schema_after_migration = false
-  config.action_mailer.default_url_options = { host: 'https://morning-tundra-66078.herokuapp.com/'}
-  config.action_mailer.delivery_method = :letter_opener_web
 end
