740b7f43252abdade2ea6f373b34d655679bfa2f
yuki fujisawa <fujisawa.yuki@outlook.com>
2021-11-25 16:58:56 +0900

    写真投稿時のバリデーション修正

--- app/models/picture.rb
+++ app/models/picture.rb
@@ -1,5 +1,8 @@
 class Picture < ApplicationRecord
   mount_uploader :image, ImageUploader
+  validates :title,  presence: true
+  validates :content,  presence: true
+  validates :image,  presence: true
   belongs_to :user
   has_many :favorites, dependent: :destroy
   has_many :favorite_users, through: :favorites, source: :user
--- app/models/user.rb
+++ app/models/user.rb
@@ -5,6 +5,7 @@ class User < ApplicationRecord
                     format: { with: /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i }
   before_validation { email.downcase! }
   has_secure_password
+  validates :image, presence: true
   validates :password, length: { minimum: 6 }
   has_many :pictures
   has_many :favorites, dependent: :destroy
